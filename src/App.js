69a1fe1b5c317692219aab96a5ae6663d85108a7
RE-JOY <rjmern@gmail.com>
2024-09-09 17:26:37 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         type="text"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a new To-Do"
+        placeholder="Add New To-Do Here"
       />
       <button onClick={handleAddTodo}>Add</button>
       <TodoList
